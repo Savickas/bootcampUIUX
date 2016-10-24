cf1b00817f28ccf4a8a774eb8c1f84ab898f56f4
Savickas <Savickas@users.noreply.github.com>
2016-10-24 23:22:30 +0200

    Add files via upload

---
+++ layout kurs html/script.js
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+$('.menu').click(function() {
+    $('header').fadeToggle()    
+})
+=======
+$('.menu').click(function() {
+    $('header').fadeToggle()    
+})
+>>>>>>> b5c100579fa31d3d84ec76c5adf7a10fb3813b45
