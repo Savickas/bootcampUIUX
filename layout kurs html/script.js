2f8454034d0eac1831461b02a3d9d001c6d15fed
Marcin Wardziński <Savickas@users.noreply.github.com>
2017-07-18 17:16:28 +0200

    Update script.js

--- layout kurs html/script.js
+++ layout kurs html/script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 $('.menu').click(function() {
     $('header').fadeToggle()    
 })
-=======
-$('.menu').click(function() {
-    $('header').fadeToggle()    
-})
->>>>>>> b5c100579fa31d3d84ec76c5adf7a10fb3813b45
